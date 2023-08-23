d256e4ef927a337f555750c8cd20d621253bd422
elieppr <eliana.raizman@hotmail.com>
2023-08-23 10:56:31 -0700

    added half fishfood button and text for timecontrol

--- evolutionProject/Assets/Scripts/TimeControl.cs
+++ evolutionProject/Assets/Scripts/TimeControl.cs
@@ -1,13 +1,23 @@
+using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
 
 public class TimeControl : MonoBehaviour
 {
     public Slider timeSlider;
+    public TMP_Text counterText;
     public void SetTimeScale(float newScale)
     {
         Time.timeScale = newScale;
         Debug.Log("NEWWWWWWWWWWWWWWWWWWWWWSCALE: " + newScale);
+
+        UpdateCounterText(newScale);
+    }
+
+
+    public void UpdateCounterText(float newScale)
+    {
+        counterText.text = $"{newScale}";
     }
 }
 
